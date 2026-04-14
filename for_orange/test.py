643173b1f41e92784a8eebf247289c65a60b77f1
daaaniel <daniilkvakin@gmail.com>
2026-04-14 23:57:54 +0300

    рабочий тестовый вариант yolo на orange pi

--- for_orange/test.py
+++ for_orange/test.py
@@ -2,6 +2,9 @@ import cv2
 from rknnlite.api import RKNNLite
 import numpy as np
 
+TRASHHOLD = 0.2
+CLASS = [0, 1, 2]
+
 INPUT_SIZE = 1280
 RKNN_MODEL = 'best_noquant.rknn'
 
@@ -17,14 +20,12 @@ print('done')
 ori_img = cv2.imread('img_gel_144_rotated.jpg')
 img = cv2.resize(ori_img, (INPUT_SIZE, INPUT_SIZE))
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = np.transpose(img, (2, 0, 1))
 img = np.expand_dims(img, 0)       
-img = img.astype(np.float32) / 255.0  
-
-print(img.shape)
 
 ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_ALL)
 
-outputs = rknn_lite.inference(inputs=[img], data_format='nchw')
+outputs = rknn_lite.inference(inputs=[img], data_format='nhwc')
+
+print([x for x in outputs[0][0] if x[-2] > TRASHHOLD and x[-1] in ])
 
 rknn_lite.release()
